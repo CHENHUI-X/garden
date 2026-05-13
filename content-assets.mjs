1395c93276d6980aa28f4d0924798852934dc3c2
CHENHUI-X <CHENHUI-X@users.noreply.github.com>
2026-05-13 11:31:51 +0000

    deploy: adb4ffaee0e64e3a1e765e2174bfb71c6dace74d

--- content-assets.mjs
+++ content-assets.mjs
@@ -1,5 +1,5 @@
 
-import __ASTRO_IMAGE_IMPORT_Z1qewVz from "../../../assets/customize_theme.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fja%2Fcustom.md";
 import __ASTRO_IMAGE_IMPORT_ZVy1LR from "../../../assets/customize_theme.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fzh-cn%2Fcustom.md";
-export default new Map([["../../../assets/customize_theme.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fja%2Fcustom.md", __ASTRO_IMAGE_IMPORT_Z1qewVz], ["../../../assets/customize_theme.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fzh-cn%2Fcustom.md", __ASTRO_IMAGE_IMPORT_ZVy1LR]]);
+import __ASTRO_IMAGE_IMPORT_Z1qewVz from "../../../assets/customize_theme.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fja%2Fcustom.md";
+export default new Map([["../../../assets/customize_theme.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fzh-cn%2Fcustom.md", __ASTRO_IMAGE_IMPORT_ZVy1LR], ["../../../assets/customize_theme.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fja%2Fcustom.md", __ASTRO_IMAGE_IMPORT_Z1qewVz]]);
 		
